9047f6651245015b3e940dd64ba60d244af7739b
rosol111 <gulamrosul037@gmail.com>
2023-11-24 00:10:48 +0600

    Added display different menu catedory items with cover

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,11 +1,19 @@
+/* eslint-disable react/prop-types */
 
+import Cover from "../../../Shared/Cover/Cover";
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title, Img }) => {
+  return (
+    <div className="pt-8">
+      {title && <Cover img={Img} title ={title}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 my-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
